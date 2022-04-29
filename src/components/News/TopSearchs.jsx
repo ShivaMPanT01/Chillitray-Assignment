bce1ed79c18f95b1f379d49322d5e970ee11b1bd
ShiivaM PanT <shubhampant1596@gmail.com>
2022-04-29 19:32:00 +0530

    TopSearch: Create Top Search.

---
+++ src/components/News/TopSearchs.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import styles from '../../styles/TopSearchs.module.css';
+import { topSearchs } from '../../script/data';
+
+function TopSearchs() {
+   return (
+      <div className={styles['top-searchs']}>
+         <h2>Top Searchs for you</h2>
+         <div className={styles['top-searchs__content']}>
+            <ul className={styles['top-searchs__list']}>
+               {topSearchs.map((search) => {
+                  return (
+                     <li key={search} className={styles['top-searchs__item']}>
+                        {search}
+                     </li>
+                  );
+               })}
+            </ul>
+            <div className={styles.dots}>
+               <span className={styles['dot']}></span>
+               <span className={styles['dot']}></span>
+               <span className={styles['dot']}> </span>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default TopSearchs;
---
+++ src/script/data.js
@@ -0,0 +1,210 @@
+import HotScoops from '../Assets/images/HotScoops.png';
+import Earnings from '../Assets/images/Earnings.png';
+import Expansion from '../Assets/images/Expansion.png';
+import Infrastructure from '../Assets/images/Infrastructure.png';
+import Legal from '../Assets/images/Legal.png';
+import Marketing from '../Assets/images/Marketing.png';
+import NewHire from '../Assets/images/NewHire.png';
+import OpenPositions from '../Assets/images/OpenPositions.png';
+import Technology from '../Assets/images/Technology.png';
+import Budgeting from '../Assets/images/Budgeting.png';
+import Awards from '../Assets/images/Awards.png';
+import Analytics from '../Assets/images/Analytics.png';
+import Communications from '../Assets/images/Communications.png';
+
+import Like from '../Assets/images/Like.png';
+import Pocket from '../Assets/images/pocket.png';
+import Share from '../Assets/images/share.png';
+import Phone from '../Assets/images/Phone.png';
+
+export const sideBarList = [
+   {
+      id: '1d',
+      name: 'Hot Scoops',
+      img: HotScoops,
+   },
+   {
+      id: '2d',
+      name: 'Earnings',
+      img: Earnings,
+   },
+   {
+      id: '3d',
+      name: 'Awards',
+      img: Awards,
+   },
+   {
+      id: '4d',
+      name: 'Expansion',
+      img: Expansion,
+   },
+   {
+      id: '5d',
+      name: 'Open Positions',
+      img: OpenPositions,
+   },
+   {
+      id: '6d',
+      name: 'New Hire',
+      img: NewHire,
+   },
+   {
+      id: '7d',
+      name: 'Analytics & Reporting',
+      img: Analytics,
+   },
+   {
+      id: '8d',
+      name: 'Budgeting & Planning',
+      img: Budgeting,
+   },
+   {
+      id: '9d',
+      name: 'Communications',
+      img: Communications,
+   },
+   {
+      id: '10d',
+      name: 'Technology',
+      img: Technology,
+   },
+   {
+      id: '11d',
+      name: 'Legal',
+      img: Legal,
+   },
+   {
+      id: '12d',
+      name: 'Infrastructure',
+      img: Infrastructure,
+   },
+   {
+      id: '13d',
+      name: 'Marketing',
+      img: Marketing,
+   },
+];
+
+export const topSearchs = [
+   'All',
+   'Finance',
+   'Buisness',
+   'Oppurtunity',
+   'Personal',
+   'Company',
+];
+
+export const newsData = [
+   {
+      id: '1a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+   {
+      id: '2a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+   {
+      id: '3a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+   {
+      id: '4a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+   {
+      id: '5a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+   {
+      id: '6a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+   {
+      id: '7a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+   {
+      id: '8a',
+      title: 'Hot Scoops',
+      heading:
+         'Samsung Galaxy F22 launched in India: Price, features, other details',
+      content:
+         'Samsung Galaxy F22 has been launched in India. The new smartphone has been priced in the mid-range segment. The new smartphone is powered by a MediaTek chipset and features a high refresh rate AMOLED display.',
+      img: {
+         phone: Phone,
+         like: Like,
+         pocket: Pocket,
+         share: Share,
+      },
+   },
+];
