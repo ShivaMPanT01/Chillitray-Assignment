32d2548d1e46d3f8200cd0e61b8708308ab28173
ShiivaM PanT <shubhampant1596@gmail.com>
2022-04-30 08:31:28 +0530

    HomePage : Create Home Page.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import TopSearchs from './News/TopSearchs';
+import TopSearches from './News/TopSearches';
 import SideBar from './SideBar';
 import styles from '../styles/HomePage.module.css';
 import News from './News/News';
+import RecentSearchs from './RecentSearchs';
+import Follow from './Follow';
 
 function HomePage() {
    return (
       <div className={styles.container}>
          <SideBar />
          <div>
-            <TopSearchs />
+            <TopSearches />
             <News />
+         </div>
+         <div>
+            <RecentSearchs />
+            <Follow />
          </div>
       </div>
    );
